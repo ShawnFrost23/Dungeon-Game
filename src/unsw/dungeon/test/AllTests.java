9d2e971ae531fa39077a52672b1007c107086e97
nick-killeen <nicholas.killeen2@gmail.com>
2019-10-25 23:57:36 +1100

    Added autotest schema

---
+++ src/unsw/dungeon/test/AllTests.java
@@ -0,0 +1,10 @@
+package unsw.dungeon.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({TestBoulder.class, TestEnemies.class})
+public class AllTests {
+}
---
+++ src/unsw/dungeon/test/TestBoulder.java
@@ -0,0 +1,44 @@
+package unsw.dungeon.test;
+
+import static org.junit.Assert.fail;
+import org.junit.Test;
+
+public class TestBoulder {
+	/**
+	 * "Boulders are loaded from the map file and rendered in their correct
+	 * position."
+	 */
+	@Test
+	public void AC1() {
+		fail("Not implemented!");
+	}
+	
+	/**
+	 * "Can push boulders into adjacent squares by walking into from the
+	 * opposite direction."
+	 */
+	@Test
+	public void AC2() {
+		fail("Not implemented!");
+	}
+	
+	/**
+	 * "Can only push boulders if there are no walls or other boulders in its
+	 * path. Boulders otherwise cannot be walked into (behave like walls)."
+	 */
+	@Test
+	public void AC3() {
+		fail("Not implemented!");
+	}
+	
+	
+	/**
+	 * "Boulders cannot be pushed into enemies. Pushing boulders on top of a
+	 * pickup-item (sword, key, or invincibility potion ...) will result in
+	 * that item's destruction."
+	 */
+	@Test
+	public void AC4() {
+		fail("Not implemented!");
+	}
+}
