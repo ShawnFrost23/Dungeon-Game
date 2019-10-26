aaeceb8b0da034e5f710f69519186422e50ef5bb
nick-killeen <nicholas.killeen2@gmail.com>
2019-10-26 15:36:46 +1100

    Added Direction docs

--- src/unsw/dungeon/back/Direction.java
+++ src/unsw/dungeon/back/Direction.java
@@ -1,5 +1,9 @@
 package unsw.dungeon.back;
 
+/**
+ * A direction that {@link MoveableEntity MoveableEntities} can "act" in (move,
+ * push, swing sword).
+ */
 public enum Direction {
 	UP, DOWN, LEFT, RIGHT
 }
