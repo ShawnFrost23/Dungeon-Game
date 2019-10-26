d662c13ad46e7961ffabcf9bb7b3cbbff59a12bf
nick-killeen <nicholas.killeen2@gmail.com>
2019-10-26 15:55:35 +1100

    Added Cell docs
    I've always felt a little bit unhappy with this class, I will probably
    shortly be changing it.

--- src/unsw/dungeon/back/Cell.java
+++ src/unsw/dungeon/back/Cell.java
@@ -3,6 +3,13 @@ package unsw.dungeon.back;
 import java.util.ArrayList;
 import java.util.List;
 
+// TODO: enforce "Cell's shouldn't expose any notion of coordinates".
+// TODO: add push(Dir) method here.
+// TODO: ... a lot of redesign here.
+
+/**
+ * A collection of Entities at one discrete location.
+ */
 public class Cell {
 	private Board board;
 	private int x;
@@ -10,6 +17,12 @@ public class Cell {
 	
 	private List<Entity> entities;
 	
+	/**
+	 * Construct a new Cell.
+	 * @param board board to which this cell belongs
+	 * @param x x-coordinate of the cell
+	 * @param y y-cooridnate of the cell
+	 */
 	public Cell(Board board, int x, int y) {
 		this.x = x;
 		this.y = y;
@@ -17,20 +30,40 @@ public class Cell {
 		this.entities = new ArrayList<Entity>();
 	}
 
+	/**
+	 * Get this Cell's x-coordinate. This function should only be used by Board;
+	 * Cell's shouldn't expose any notion of coordinates, just know what is
+	 * directly adjacent to them.
+	 * @return this Cell's x-coordinate
+	 */
 	public int getX() {
 		return this.x;
 	}
+	/**
+	 * Get this Cell's y-coordinate. This function should only be used by Board;
+	 * Cell's shouldn't expose any notion of coordinates, just know what is
+	 * directly adjacent to them.
+	 * @return this Cell's y-coordinate
+	 */
 	public int getY() {
 		return this.y;
 	}
 	
+	/**
+	 * Add an entity to this cell. Should only be used to create a Board.
+	 * @param e entity to add
+	 */
 	public void addEntity(Entity e) {
 		this.entities.add(e);
 	}
-	public void removeEntity(Entity e) {
-		this.entities.remove(e);
-	}
 
+	/**
+	 * Get the texture for this cell. This will be the texture of the entity on
+	 * this cell with the largest z-index, or ' ' if none are present.
+	 * @return this cell's texture
+	 * @see {@link Entity#getTexture()}
+	 * @see {@link Entity#getZ()}
+	 */
 	public char getTexture() {
 		if (this.entities.size() == 0) {
 			return ' ';
@@ -48,10 +81,22 @@ public class Cell {
 		return texture;
 	}
 	
+	/**
+	 * Get the cell that is adjacent to this one.
+	 * @param d direction to get adjacent in
+	 * @return the cell that is adjacent to this one in direction <b>d</b>
+	 */
 	public Cell adjacent(Direction d) {
 		return board.adjacent(this, d);
 	}
 	
+	/**
+	 * True if this Cell will prevent an entry attempt by <b>m</b> -- <b>m</b>
+	 * cannot walk atop of it.s
+	 * @param m MoveableEntity to whether or not en
+	 * @param d
+	 * @return
+	 */
 	public boolean willPreventEntry(MoveableEntity m, Direction d) {
 		for (Entity entity : this.entities) {
 			if (entity.willPreventEntry(m, d)) {
@@ -60,14 +105,27 @@ public class Cell {
 		}
 		return false;
 	}
-	
+
+	/**
+	 * Notify all entities on this Cell that MoveableEntity <b>m</b> has just
+	 * left. Furthermore, declare to this Cell itself that <b>m</b> has left.
+	 * @param m MoveableEntity that left this Cell
+	 * @param d Direction they left in
+	 */
 	public void exit(MoveableEntity m, Direction d) {
 		this.entities.remove(m);
 		for (Entity entity : new ArrayList<Entity>(this.entities)) {
 			entity.onExit(m, d);
 		}
 	}
-	
+
+	/**
+	 * Notify all entities on this Cell that MoveableEntity <b>m</b> has just
+	 * entered. Furthermore, declare to this Cell itself that <b>m</b> has
+	 * entered.
+	 * @param m MoveableEntity that entered this Cell
+	 * @param d Direction they came from
+	 */
 	public void enter(MoveableEntity m, Direction d) {
 		for (Entity entity : new ArrayList<Entity>(this.entities)) {
 			entity.onEnter(m, d);
