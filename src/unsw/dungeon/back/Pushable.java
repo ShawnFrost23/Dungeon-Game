ebf61178706b18b7a67ed58b6d0c04d438829fe4
nick-killeen <nicholas.killeen2@gmail.com>
2019-10-26 18:30:13 +1100

    Added Pushable docs

--- src/unsw/dungeon/back/Pushable.java
+++ src/unsw/dungeon/back/Pushable.java
@@ -1,5 +1,10 @@
 package unsw.dungeon.back;
 
+/**
+ * Entities that trigger an action when they are "pushed". A push occurs when a
+ * Player attempts to move into a Cell, before the player tries to actually take
+ * a step in that direction.
+ */
 public interface Pushable extends Entity {
 	/**
 	 * Push this entity in a particular direction.
