0bdb84d9e07c15d9989242b4f7539f9603252693
nick-killeen <nicholas.killeen2@gmail.com>
2019-10-26 12:22:59 +1100

    Added tiny dungeon creation test that fails

---
+++ src/unsw/dungeon/test/TestBasics.java
@@ -0,0 +1,53 @@
+package unsw.dungeon.test;
+
+import static org.junit.Assert.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
+
+import unsw.dungeon.back.*;
+
+/**
+ * Tests for basic game engine things that don't really fit in any user story.
+ */
+public class TestBasics {
+	
+	/**
+	 * Trying to make a N x 1 dungeon failed at some point in the past. This
+	 * test checks that this and related cases are correctly implemented.
+	 */
+	@Test
+	public void testTinyDungeons() {
+		Game g1 = Game.createGame(""
+			+ "  P  \n"
+		);
+		
+		assertEquals(g1.getBoardString(), ""
+			+ "  P  \n"
+		);
+		
+		Game g2 = Game.createGame(""
+			+ " \n"
+			+ " \n"
+			+ "P\n"
+			+ " \n"
+		);
+		
+		assertEquals(g2.getBoardString(), ""
+			+ " \n"
+			+ " \n"
+			+ "P\n"
+			+ " \n"
+		);
+		
+		Game g3 = Game.createGame(""
+			+ "P\n"
+		);
+		
+		assertEquals(g3.getBoardString(), ""
+			+ "P\n"
+		);
+		
+	}
+
+}
