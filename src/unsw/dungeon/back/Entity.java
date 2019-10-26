33663c3b08aedac42a0391c41e97a13ca79af75d
nick-killeen <nicholas.killeen2@gmail.com>
2019-10-26 18:41:42 +1100

    Added docs for Entity interface

--- src/unsw/dungeon/back/Entity.java
+++ src/unsw/dungeon/back/Entity.java
@@ -1,6 +1,20 @@
 package unsw.dungeon.back;
 
+/**
+ * Something that can be put into a Cell.
+ */
 public interface Entity {
+	/**
+	 * Get the z-index for this Entity. An Entity with a larger z-index will be
+	 * drawn on top of an Entity with a smaller z-index.
+	 * @return the z-index for this Entity
+	 * @see {@link Cell#getTexture()}
+	 */
 	public int getZ();
+	/**
+	 * Get the texture this Entity should be displayed with.
+	 * @return texture to display this Entity with
+	 * @see {@link Cell#getTexture()}
+	 */
 	public char getTexture();
 }
