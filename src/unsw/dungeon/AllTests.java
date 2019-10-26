0fa02b4ae4b49b6421999d9f467c58b27d155904
nick-killeen <nicholas.killeen2@gmail.com>
2019-10-26 12:12:01 +1100

    AllTests searches for tests in package
    Previously, with a classlist, it's not unlikely that we would write a
    new test class and forget to add it to the list.

--- src/unsw/dungeon/AllTests.java
+++ src/unsw/dungeon/AllTests.java
@@ -1,10 +1,11 @@
-package unsw.dungeon.test;
+package unsw.dungeon;
 
 import org.junit.platform.runner.JUnitPlatform;
-import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.SelectPackages;
 import org.junit.runner.RunWith;
 
 @RunWith(JUnitPlatform.class)
-@SelectClasses({TestBoulder.class, TestEnemies.class})
+@SelectPackages("unsw.dungeon.test")
 public class AllTests {
+	
 }
