559510f8a0616984b7de8bc4ad665fc793bdc0bf
nick-killeen <nicholas.killeen2@gmail.com>
2019-10-28 10:57:57 +1100

    FloorSwitches detect boulder movement internally
    They detect it, but do not notify anyone.

--- src/unsw/dungeon/back/FloorSwitch.java
+++ src/unsw/dungeon/back/FloorSwitch.java
@@ -1,7 +1,26 @@
 package unsw.dungeon.back;
 
-public class FloorSwitch implements Entity {
+import unsw.dungeon.back.event.CellEnteredEvent;
+import unsw.dungeon.back.event.CellExitedEvent;
+import unsw.dungeon.back.event.Event;
+import unsw.dungeon.back.event.Observer;
 
+public class FloorSwitch implements Entity, Observer {
+	private boolean isPressed;
+
+	public FloorSwitch() {
+		 // TODO: isn't false if it has a boulder on top of it to begin with.
+		this.isPressed = false;
+	}
+	
+	public void press() {
+		this.isPressed = true;
+	}
+	
+	public void unpress() {
+		this.isPressed = false;
+	}
+	
 	@Override
 	public int getZ() {
 		return 0;
@@ -10,6 +29,27 @@ public class FloorSwitch implements Entity {
 	@Override
 	public char getTexture() {
 		return '_';
+	}
+
+	@Override
+	public void notifyOf(Event event) {
+		if (event instanceof CellEnteredEvent) {
+			this.onEnter((CellEnteredEvent) event);
+		} else if (event instanceof CellExitedEvent) {
+			this.onExit((CellExitedEvent) event); 
+		}
+	}
+
+	private void onEnter(CellEnteredEvent event) {
+		if (event.getWhoEntered() instanceof Boulder) {
+			this.press();
+		}
+	}
+	
+	private void onExit(CellExitedEvent event) {
+		if (event.getWhoExited() instanceof Boulder) {
+			this.unpress();
+		}
 	}
 
 }
