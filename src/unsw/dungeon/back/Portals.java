f58a8203be5caf86f1b6b853a02296693ddaf0ba
Arth Patel <arthpatelf50@gmail.com>
2019-10-29 14:56:51 +1100

    Constructors, getters and setter for some stuff. ** Constructor may have to be changed later to just set the location of pair portals **

--- src/unsw/dungeon/back/Portals.java
+++ src/unsw/dungeon/back/Portals.java
@@ -5,9 +5,27 @@ public class Portals implements Entity {
     private Cell location;
     private Portals pairPortal;
 
+    public Portals(Cell location, Cell pairPortalLocation) {
+        this.location = location;
+        this.pairPortal.setLocation(pairPortalLocation);
 
+    }
 
+    public Cell getLocation() {
+        return location;
+    }
 
+    public void setLocation(Cell location) {
+        this.location = location;
+    }
+
+    public Portals getPairPortal() {
+        return pairPortal;
+    }
+
+    public void setPairPortal(Portals pairPortal) {
+        this.pairPortal = pairPortal;
+    }
 
 
 
