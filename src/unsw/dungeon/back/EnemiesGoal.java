f779500f4ac21ed7f69dc68907ce3e82310075a0
nick-killeen <nicholas.killeen2@gmail.com>
2019-10-30 22:50:29 +1100

    Stub EnemiesGoal function so that tests compile

---
+++ src/unsw/dungeon/back/EnemiesGoal.java
@@ -0,0 +1,25 @@
+package unsw.dungeon.back;
+
+import unsw.dungeon.back.event.Event;
+
+public class EnemiesGoal implements Goal {
+
+	@Override
+	public void notifyOf(Event event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isSatisfied() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void trackEntity(Entity e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
