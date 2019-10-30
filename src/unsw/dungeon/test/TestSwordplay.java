29f1646b7b6cab39d7e03cd0ccc5a2f884fd12a9
nick-killeen <nicholas.killeen2@gmail.com>
2019-10-30 22:53:32 +1100

    Added Swordplay AC1-8 autotest implementations
    And removed lousy todo comment + dumplicate test.

--- src/unsw/dungeon/test/TestSwordplay.java
+++ src/unsw/dungeon/test/TestSwordplay.java
@@ -17,18 +17,39 @@ public class TestSwordplay {
 	public void AC1() {
 		Game g = Game.createGame(new ImpossibleGoal(), ""
 			+ " P S\n"
+			+ "   S\n"
+			+ " S  \n"
 		);
-		fail("Test not implemented!");
+		
+		assertEquals(""
+			+ " P S\n"
+			+ "   S\n"
+			+ " S  \n"
+			, g.getBoardString()
+		);
+		
 	}
 	
 	/**
 	 * "A player can pick up a sword by walking over it (if they are not holding
 	 * one already), and their player avatar changes to indicate they are
 	 * holding sword."
+	 * 
+	 * "Player avatar changes" isn't tested.
 	 */
 	@Test
 	public void AC2() {
-		fail("Test not implemented!");
+		Game g = Game.createGame(new ImpossibleGoal(), ""
+			+ " PS \n"
+		);
+		
+		g.movePlayer(Direction.RIGHT);
+		g.movePlayer(Direction.RIGHT);
+		
+		assertEquals(""
+			+ "   P\n"
+			, g.getBoardString()
+		);
 	}
 	
 	/**
@@ -38,16 +59,43 @@ public class TestSwordplay {
 	 */
 	@Test
 	public void AC3() {
-		fail("Test not implemented!");
+		Game g = Game.createGame(new ImpossibleGoal(), ""
+			+ " P S S \n"
+		);
+		
+		g.movePlayer(Direction.RIGHT);
+		g.movePlayer(Direction.RIGHT);
+		g.movePlayer(Direction.RIGHT);
+		g.movePlayer(Direction.RIGHT);
+		g.movePlayer(Direction.RIGHT);
+		
+		assertEquals(""
+			+ "     SP\n"
+			, g.getBoardString()
+		);
 	}
 	
 	/**
 	 * "If a player has a sword, they can press one of "w, a, s, d" to attack
 	 * the tile immediately to their left, bottom, right, top."
+	 * 
+	 * This test doesn't really test anything ...
 	 */
 	@Test
 	public void AC4() {
-		fail("Test not implemented!");
+		Game g = Game.createGame(new ImpossibleGoal(), ""
+			+ "   \n"
+			+ " P \n"
+			+ "   \n"
+			, ""
+			+ "   \n"
+			+ " S \n"
+			+ "   \n"
+		);
+		g.swingSword(Direction.UP);
+		g.swingSword(Direction.DOWN);
+		g.swingSword(Direction.LEFT);
+		g.swingSword(Direction.RIGHT);
 	}
 	
 	/**
@@ -56,7 +104,51 @@ public class TestSwordplay {
 	 */
 	@Test
 	public void AC5() {
-		fail("Test not implemented!");
+		Game g = Game.createGame(new ImpossibleGoal(), ""
+			+ "    \n"
+			+ "EEP \n"
+			+ " E  \n"
+			, ""
+			+ "    \n"
+			+ "  S \n"
+			+ "    \n"
+		);
+		
+		g.swingSword(Direction.LEFT);
+		
+		assertEquals(""
+			+ "    \n"
+			+ "E P \n"
+			+ " E  \n"
+			, g.getBoardString()
+		);
+		
+		g.movePlayer(Direction.LEFT);
+		
+		assertFalse(g.getHasLost());
+		
+		g.swingSword(Direction.LEFT);
+		
+		assertEquals(""
+			+ "    \n"
+			+ " P  \n"
+			+ " E  \n"
+			, g.getBoardString()
+		);		
+		
+		g.swingSword(Direction.DOWN);
+		
+		assertEquals(""
+			+ "    \n"
+			+ " P  \n"
+			+ "    \n"
+			, g.getBoardString()
+		);
+		
+		g.moveEnemies();
+		
+		assertFalse(g.getHasLost());
+		
 	}
 	 
 	/**
@@ -66,7 +158,48 @@ public class TestSwordplay {
 	 */
 	@Test
 	public void AC6() {
-		fail("Test not implemented!");
+		Game g = Game.createGame(new ImpossibleGoal(), ""
+			+ " E \n"
+			+ "SPE\n"
+			+ " W \n"
+			, ""
+			+ "   \n"
+			+ " S \n"
+			+ "   \n"
+		);
+		
+		g.swingSword(Direction.LEFT);
+		g.swingSword(Direction.LEFT);
+		g.swingSword(Direction.DOWN);
+		g.swingSword(Direction.DOWN);
+		g.swingSword(Direction.RIGHT);
+	
+		assertEquals(""
+			+ " E \n"
+			+ "SP \n"
+			+ "   \n"
+			, g.getBoardString()
+		);
+		
+		g.swingSword(Direction.UP);
+		
+		assertEquals(""
+			+ " E \n"
+			+ "SP \n"
+			+ "   \n"
+			, g.getBoardString()
+		);
+		
+		g.movePlayer(Direction.LEFT);
+		g.movePlayer(Direction.RIGHT);
+		g.swingSword(Direction.UP);
+		
+		assertEquals(""
+			+ "   \n"
+			+ " P \n"
+			+ "   \n"
+			, g.getBoardString()
+		);
 	}
 
 	/**
@@ -75,30 +208,83 @@ public class TestSwordplay {
 	 */
 	@Test
 	public void AC7() {
-		fail("Test not implemented!");
+		Game g = Game.createGame(new ImpossibleGoal(), ""
+			+ " EEEPE\n"
+			, ""
+			+ "    S \n"
+			+ "    S \n"
+		);
+		
+		for (int i = 0; i < 5; ++i) {
+			g.swingSword(Direction.RIGHT);
+		}
+
+		assertEquals(""
+			+ " EEEP \n"
+			, g.getBoardString()
+		);
+		
+		g.swingSword(Direction.LEFT);
+		
+		assertEquals(""
+			+ " EE P \n"
+			, g.getBoardString()
+		);
+		
+		g.movePlayer(Direction.LEFT);
+		
+		assertEquals(""
+			+ " EEP  \n"
+			, g.getBoardString()
+		);
+		
+		for (int i = 0; i < 4; ++i) {
+			g.swingSword(Direction.LEFT);
+		}
+		
+		g.movePlayer(Direction.LEFT);
+		g.swingSword(Direction.LEFT);
+		
+		assertEquals(""
+			+ " EP   \n"
+			, g.getBoardString()
+		);
 	}
 
 	/**
-	 * "Every swing of the sword, regardless of whether it hits, causes the
-	 * sword to lose one durability. A sword disappears when it loses all of its
-	 * durability. Each sword begins with 5 durability."
+	 * If all enemies have been defeated when an enemy-destruction goal is
+	 * specified, the player wins the game.
 	 */
 	@Test
 	public void AC8() {
-		fail("Test not implemented!");
+		Game g = Game.createGame(new EnemiesGoal(), ""
+			+ "   EP E\n"
+			, ""
+			+ "     S \n"
+		);
+		
+		assertFalse(g.getHasWon());
+		
+		g.swingSword(Direction.LEFT);
+		
+		assertFalse(g.getHasWon());
+		
+		g.movePlayer(Direction.RIGHT);
+		g.swingSword(Direction.RIGHT);
+		
+		assertFalse(g.getHasWon());
+		
+		g.movePlayer(Direction.LEFT);
+		
+		assertEquals(""
+			+ "   EP  \n"
+			, g.getBoardString()
+		);
+		
+		assertFalse(g.getHasWon());
+		
+		g.swingSword(Direction.LEFT);
+		
+		assertTrue(g.getHasWon());
 	}
-
-	/**
-	 *  If all enemies have been defeated when an enemy-destruction goal is
-	 *  specified, the player wins the game.
-	 */
-	@Test
-	public void AC9() {
-		fail("Test not implemented!");
-	}
-	
-	// TODO: also test that swords stood atop of are automatically picked up
-	// if you perform your last sting while standing atop a new sword.
-	// Viz., "sword-break" should try to instigate a sword-pickup.
-
 }
