d12924865d11b4fd50698a5029b8e592248a06ca
nick-killeen <nicholas.killeen2@gmail.com>
2019-10-31 10:21:31 +1100

    Swordplay AC8 passed; EnemiesGoal class built
    All tests passed! Though refactoring WILL be required later, the
    badness should be reasonably self-contained and not problematic in the
    foreseeable future.

--- src/unsw/dungeon/back/EnemiesGoal.java
+++ src/unsw/dungeon/back/EnemiesGoal.java
@@ -1,25 +1,34 @@
 package unsw.dungeon.back;
 
+import unsw.dungeon.back.event.EnemyKilledEvent;
 import unsw.dungeon.back.event.Event;
+import unsw.dungeon.back.event.Observer;
 
-public class EnemiesGoal implements Goal {
+public class EnemiesGoal implements Goal, Observer {
+	private int numEnemiesRemaining;
+	
+	public EnemiesGoal() {
+		this.numEnemiesRemaining = 0;
+	}
 
 	@Override
 	public void notifyOf(Event event) {
-		// TODO Auto-generated method stub
-		
+		if (event instanceof EnemyKilledEvent) {
+			this.numEnemiesRemaining -= 1;
+		}
 	}
 
 	@Override
 	public boolean isSatisfied() {
-		// TODO Auto-generated method stub
-		return false;
+		return this.numEnemiesRemaining == 0;
 	}
 
 	@Override
 	public void trackEntity(Entity e) {
-		// TODO Auto-generated method stub
-		
+		if (e instanceof Enemy) {
+			((Enemy) e).attachListener(this);
+			this.numEnemiesRemaining += 1;
+		}
 	}
 
 }
