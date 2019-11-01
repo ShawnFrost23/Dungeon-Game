76c0940b9b8e97a129d1b34fa38342b0522855a0
nick-killeen <nicholas.killeen2@gmail.com>
2019-11-01 23:26:28 +1100

    Added stub portal test.

---
+++ src/unsw/dungeon/test/TestPortals.java
@@ -0,0 +1,44 @@
+package unsw.dungeon.test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+
+public class TestPortals {
+	/**
+	 * "Portals are loaded from the map file and rendered correctly."
+	 */ 
+	@Test
+	public void AC1() {
+		fail("Test not implemented");
+	}
+	
+	/**
+	 * "... That is, entering a portal behaves as though the player was
+	 * initially standing on the other portal tile and is moving out of
+	 * it, so long as it is possible to do so. "
+	 */ 
+	@Test
+	public void AC2() {
+		fail("Test not implemented");
+	}
+	
+	/**
+	 * "Enemies will treat portals like walls and not attempt to use them."
+	 */
+	@Test
+	public void AC3() {
+		fail("Test not implemented");
+	}
+	
+	/**
+	 * Portals come in pairs and can be used from either end.
+	 */
+	@Test
+	public void AC4() {
+		fail("Test not implemented");
+	}
+}
