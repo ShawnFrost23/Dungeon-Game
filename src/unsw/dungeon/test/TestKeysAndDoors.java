0cf7525175a674316e48967a6e323053d908923d
Arth Patel <arthpatelf50@gmail.com>
2019-11-01 13:19:01 +1100

    Basic tests for the keys and doors

---
+++ src/unsw/dungeon/test/TestKeysAndDoors.java
@@ -0,0 +1,113 @@
+package unsw.dungeon.test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+import unsw.dungeon.back.*;
+import unsw.dungeon.spoof.ImpossibleGoal;
+
+/**
+ * Tests for the Keys and Doors user story
+ */
+public class TestKeysAndDoors {
+    /**
+     * "Keys and doors are loaded from the map file and rendered in their correct
+     * position."
+     */
+    @Test
+    public void test01() {
+        Game g = Game.createGame(new ImpossibleGoal(), ""
+                + "P ~ W \n"
+                + "    # \n"
+                + "    W \n"
+        );
+
+        assertEquals(g.getBoardString(),""
+                + "P ~ W \n"
+                + "    # \n"
+                + "    W \n"
+        );
+    }
+
+    /**
+     * "Can Pick up keys by simply walking over them"
+     */
+    @Test
+    public void test02() {
+        Game g = Game.createGame(new ImpossibleGoal(), ""
+                + " P    \n"
+                + "      \n"
+                + "  ~   \n"
+        );
+
+        g.movePlayer(Direction.RIGHT);
+        g.movePlayer(Direction.DOWN);
+
+        assertEquals(g.getBoardString(),""
+                + "      \n"
+                + "  P   \n"
+                + "  ~   \n"
+        );
+
+        g.movePlayer(Direction.DOWN);
+
+        assertEquals(g.getBoardString(),""
+                + "      \n"
+                + "      \n"
+                + "  P   \n"
+        );
+
+        g.movePlayer(Direction.RIGHT);
+
+        assertEquals(g.getBoardString(),""
+                + "      \n"
+                + "      \n"
+                + "   P  \n"
+        );
+    }
+
+    /**
+     * "Can Pick up keys by simply walking over them"
+     */
+    @Test
+    public void test03() {
+        Game g = Game.createGame(new ImpossibleGoal(), ""
+                + " P  W  \n"
+                + " ~  #  \n"
+                + "    W  \n"
+        );
+
+        g.movePlayer(Direction.DOWN);
+        assertEquals(g.getBoardString(),""
+                + "    W  \n"
+                + " P  #  \n"
+                + "    W  \n"
+        );
+
+        g.movePlayer(Direction.RIGHT);
+        g.movePlayer(Direction.RIGHT);
+        assertEquals(g.getBoardString(),""
+                + "    W  \n"
+                + "   P#  \n"
+                + "    W  \n"
+        );
+
+        g.movePlayer(Direction.RIGHT);
+        assertEquals(g.getBoardString(),""
+                + "    W  \n"
+                + "    P  \n"
+                + "    W  \n"
+        );
+
+        g.movePlayer(Direction.RIGHT);
+        assertEquals(g.getBoardString(),""
+                + "    W  \n"
+                + "    |P \n"
+                + "    W  \n"
+        );
+
+    }
+
+
+}
