d8b1942541908eeefeccfdfaf84ea49a05a37f92
nick-killeen <nicholas.killeen2@gmail.com>
2019-11-01 22:37:31 +1100

    Fix (portion of) bad merge in WorldState.java
    How'd that get here?

--- src/unsw/dungeon/back/WorldState.java
+++ src/unsw/dungeon/back/WorldState.java
@@ -53,47 +53,4 @@ public class WorldState {
 	public int getGoalY() {
 		return this.goalLocation.getY();
 	}
-
-    public static class Treasure implements Entity, Observer {
-
-        private Cell location;
-
-        public Treasure(Cell c) {
-
-            // treasure on given cell
-            this.location = c;
-        }
-
-        public void pickUp(Player player) {
-            // Add to player list
-            player.attachListener(this);
-            // Remove from Board cell
-            this.location.removeEntity(this);
-        }
-
-        @Override
-        public int getZ() {
-            return 0;
-        }
-
-        @Override
-        public char getTexture() {
-            return '_';
-        }
-
-        @Override
-        public void notifyOf(Event event) {
-            if (event instanceof CellEnteredEvent) {
-                this.onEnter((CellEnteredEvent) event);
-            }
-
-        }
-        private void onEnter(CellEnteredEvent event) {
-            if (event.getWhoEntered() instanceof Player) {
-                //Calls pickUp
-              //  this.pickUp(event.getWhoEntered());
-            }
-        }
-
-    }
 }
