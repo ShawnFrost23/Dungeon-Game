57c7d1b785077c3e4b640a2e0cf168f2ba7f4458
nick-killeen <nicholas.killeen2@gmail.com>
2019-11-03 10:57:22 +1100

    Jdoc for PlayerStepped[On|Off]Exit events

--- src/unsw/dungeon/back/event/PlayerSteppedOffExit.java
+++ src/unsw/dungeon/back/event/PlayerSteppedOffExit.java
@@ -1,4 +1,9 @@
 package unsw.dungeon.back.event;
 
+/**
+ * Event that is fired by {@link unsw.dungeon.back.Exit} when a player steps out
+ * of its cell.
+ */
 public class PlayerSteppedOffExit implements Event {
+	
 }
--- src/unsw/dungeon/back/event/PlayerSteppedOnExit.java
+++ src/unsw/dungeon/back/event/PlayerSteppedOnExit.java
@@ -1,4 +1,8 @@
 package unsw.dungeon.back.event;
 
+/**
+ * Event that is fired by {@link unsw.dungeon.back.Exit} when a player steps
+ * into its cell.
+ */
 public class PlayerSteppedOnExit implements Event {
 }
